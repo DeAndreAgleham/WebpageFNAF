2aaf651d6b04888c05bc5a8f652463b8898a08f9
DeAndreAgleham <112994552+DeAndreAgleham@users.noreply.github.com>
2023-01-04 20:13:43 -1000

    Delete FNAF but with only HTML CSS JS directory

--- FNAF but with only HTML CSS JS/Extras/harderMode/hardGameOver/hardGameOver.js
+++
@@ -1,3 +0,0 @@
-var skull = document.getElementById("skull").autoplay = true;
-var gg = document.getElementById("gg").autoplay = true;
-var gg = document.getElementById("gg").loop = true;
--- FNAF but with only HTML CSS JS/Extras/harderMode/hardNightTime/hardNightTime.js
+++
@@ -1,1 +0,0 @@
-var nightStarting = document.getElementById("nightStarting").autoplay = true;
